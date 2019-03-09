75bdad429ccb6ab766f89b46618c3a2863cc34b2
arshiam2 <arshiam2@illinois.edu>
2019-03-08 18:09:58 -0600

    fixed router issue

--- src/api/getSecurityQuestion.js
+++ src/api/getSecurityQuestion.js
@@ -1,6 +1,5 @@
 const router = require("express").Router();
 const nodemailer = require("nodemailer");
-const router = require("express").Router();
 const { sendResponse } = require("./../utils/sendResponse");
 
 router.get("/getSecurityQuestion", async function(req, res) {
--- src/api/login.js
+++ src/api/login.js
@@ -1,7 +1,6 @@
 const router = require("express").Router();
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
-const router = require("express").Router();
 const { sendResponse } = require("./../utils/sendResponse");
 
 router.post("/login", async function(req, res) {
