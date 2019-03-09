b56e6d07059ba168df66e63a4f0b74b3ff34cc0d
Feeshay <Feeshay@DESKTOP-2NJOLSU.localdomain>
2019-03-08 20:55:07 -0600

    FINALLY IT WORKS

--- src/api/google.js
+++ src/api/google.js
@@ -1,8 +1,22 @@
 const router = require("express").Router();
 const { sendResponse } = require("./../utils/sendResponse");
+const fetch = require("node-fetch");
 
 router.post("/google", async function(req, res) {
-  sendResponse(res, 400, "TODO");
+  const results = await fetch("http://127.0.0.1:8000/google", {
+    // change this to actual server
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      tokenId: req.body.tokenId
+    })
+  });
+  const parsed = await results.json();
+  //console.log(parsed)
+  res.send(parsed);
 });
 
 module.exports = router;
