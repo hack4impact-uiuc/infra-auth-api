e7c4b3621d7d455602c0444318c79b53603b652c
Helena Chi <hchi124@gmail.com>
2019-03-09 22:44:02 -0600

    ok i think i did it better this time

--- src/api/login.js
+++ src/api/login.js
@@ -4,30 +4,34 @@ const jwt = require("jsonwebtoken");
 const { sendResponse } = require("./../utils/sendResponse");
 const API_URL = "http://127.0.0.1:8000";
 
-router.post("/login", async function(req, res) {
-  const res_email = res.body.email;
-  const res_password = res.body.password;
+async apiFetchExample => {
+  const result = await login(res.body.email, res.body.password);
+  const parsed = await result.json();
+  return parsed;
+};
 
-  fetch(API_URL + "/login", {
+router.post("/login", async function(req, res) {
+  event.preventDefault();
+  const results = await fetch(`http://localhost:8000/login`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({
-      email: res_email,
-      password: res_password
+      email: emailInput,
+      password: passwordInput
     })
-  })
-    .then(r => r.json())
-    .then(resp => {
-      if (!resp.token) {
-        sendResponse(res, 400, resp.message);
-      } else {
-        sendResponse(res, 200, resp.message, {
-          token: resp.token,
-          userID: resp.uid,
-          permission: resp.permission
-        });
-      }
-    });
+  });
+
+  await results.json().then(resp => {
+    if (!resp.token) {
+      sendResponse(res, 400, resp.message);
+    } else {
+      sendResponse(res, 200, resp.message, {
+        token: resp.token,
+        userID: resp.uid,
+        permission: resp.permission
+      });
+    }
+  });
 });
 
 module.exports = router;
