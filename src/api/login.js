43f53724cf613feac487769b374394b010959af9
hungryflamingcat <hchi124@gmail.com>
2019-03-10 09:54:46 -0500

    Making a request to server/login, sending appropriate response to client (#2)
    * api now making request to server and sending token, uid, and permission to client
    
    * took out a comment
    
    * ok i think i did it better this time

--- src/api/login.js
+++ src/api/login.js
@@ -2,9 +2,36 @@ const router = require("express").Router();
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
 const { sendResponse } = require("./../utils/sendResponse");
+const API_URL = "http://127.0.0.1:8000";
+
+async apiFetchExample => {
+  const result = await login(res.body.email, res.body.password);
+  const parsed = await result.json();
+  return parsed;
+};
 
 router.post("/login", async function(req, res) {
-  sendResponse(res, 400, "TODO");
+  event.preventDefault();
+  const results = await fetch(`http://localhost:8000/login`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      email: emailInput,
+      password: passwordInput
+    })
+  });
+
+  await results.json().then(resp => {
+    if (!resp.token) {
+      sendResponse(res, 400, resp.message);
+    } else {
+      sendResponse(res, 200, resp.message, {
+        token: resp.token,
+        userID: resp.uid,
+        permission: resp.permission
+      });
+    }
+  });
 });
 
 module.exports = router;
