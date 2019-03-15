a461a928a6083ea0a1a4ae6bb366de2cca2fb35e
Feeshay <Feeshay@DESKTOP-2NJOLSU.localdomain>
2019-03-14 20:31:00 -0500

    api never works sad

---
+++ src/api/verify.js
@@ -0,0 +1,21 @@
+const router = require("express").Router();
+const { sendResponse } = require("../utils/sendResponse");
+const fetch = require("node-fetch");
+
+router.post("/google", async function(req, res) {
+  const results = await fetch("http://localhost:8000" + "/google", {
+    // change this to actual server
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      token: req.body.token
+    })
+  });
+  const parsed = await results.json();
+  res.send(parsed);
+});
+
+module.exports = router;
