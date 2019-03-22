b8a42e45370b13605b95c23e64c9fb5c31cb7934
arshiam2 <arshiam2@illinois.edu>
2019-03-21 19:17:27 -0700

    roles status updated

--- src/api/roles.js
+++ src/api/roles.js
@@ -3,17 +3,14 @@ const router = require("express").Router();
 const { sendResponse } = require("./../utils/sendResponse");
 
 router.get("/roles", async function(req, res) {
-  const results = await fetch(SERVER_URL + "/roles", {
+  console.log(req.headers.token);
+  const results = await fetch("http://localhost:8000/roles", {
     // change this to actual server
-    method: "POST",
+    method: "GET",
     headers: {
-      Accept: "application/json",
       "Content-Type": "application/json",
       token: req.headers.token
-    },
-    body: JSON.stringify({
-      newRole: req.body.newRole
-    })
+    }
   });
   const parsed = await results.json();
   res.send(parsed);
--- src/api/rolesChange.js
+++ src/api/rolesChange.js
@@ -3,20 +3,23 @@ const router = require("express").Router();
 const { sendResponse } = require("./../utils/sendResponse");
 
 router.post("/roleschange", async function(req, res) {
-  const results = await fetch(SERVER_URL + "/rolesChange", {
+  console.log(req.body.userEmail);
+  console.log(req.body.newRole);
+
+  const results = await fetch("http://localhost:8000/rolesChange", {
     // change this to actual server
     method: "POST",
     headers: {
-      Accept: "application/json",
       "Content-Type": "application/json",
       token: req.headers.token
     },
     body: JSON.stringify({
-      userId: req.body.userId,
+      userEmail: req.body.userEmail,
       newRole: req.body.newRole
     })
   });
   const parsed = await results.json();
+  console.log(parsed);
   res.send(parsed);
 });
 
